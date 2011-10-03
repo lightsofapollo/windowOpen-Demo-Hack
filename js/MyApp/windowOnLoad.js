e6b10b18bc795fbdac8b66d39a75b855e0d6a50e
Sahaja Lal <lal@successwhich-lm.(none)>
2011-10-03 15:14:17 -0700

    Detection if window does not opeen

--- js/MyApp/windowOnLoad.js
+++ js/MyApp/windowOnLoad.js
@@ -19,43 +19,41 @@ MyApp.windowOnLoad = (function() {
         },
 
         hasPopupBlocker: function(poppedWindow) {
-            var result = false;
+          var result = false;
 
-            try {
-                if (typeof poppedWindow == 'undefined') {
-                    // Safari with popup blocker... leaves the popup window handle undefined
-                    console.log('undefined case');
-                    result = true;
-            	}
-            	else if (poppedWindow && poppedWindow.closed) {
-                	// This happens if the user opens and closes the client window...
-                	// Confusing because the handle is still available, but it's in a "closed" state.
-                	// We're not saying that the window is not being blocked, we're just saying
-                	// that the window has been closed before the test could be run.
-                	console.log('we like sugar');
-                	result = false;
-            	}
-            	else if (poppedWindow) {
-                	// This is the actual test. The client window should be fine.
-                	console.log('working well');
-                	result = false;
-            	}
-            	else {
-                	// Else we'll assume the window is not OK
-                	console.log('popup window contracted rubela.');
-                	result = true;
-            	}
-        	}
-        	catch (err) {
-            	// Could not access popup window
-        	}
+          try {
+              if (typeof poppedWindow == 'undefined') {
+                  // Safari with popup blocker... leaves the popup window handle undefined
+                  result = true;
+              }
+              else if (poppedWindow && poppedWindow.closed) {
+                  // This happens if the user opens and closes the client window...
+                  // Confusing because the handle is still available, but it's in a "closed" state.
+                  // We're not saying that the window is not being blocked, we're just saying
+                  // that the window has been closed before the test could be run.
+                  result = false;               
+              }
+              else if (poppedWindow && poppedWindow.outerWidth) {                  
+                  // This is the actual test. The client window should be fine.
+                  result = false;
+              }
+              else {
+                  // Else we'll assume the window is not OK
+                  result = true;
+              }
 
-    		return result;
+          } catch (err) {
+              if (console) {
+                 console.warn("Could not access popup window", err);
+              }
+          }
+
+          return result;
 		},
 
         checkPopupStatus: function() {
             if(this.hasPopupBlocker(this.popup)) {
-                console.log('Oh noez! There\'s no more fish!');
+                alert('Oh noez! There\'s no more fish!');
             }
         }
         
