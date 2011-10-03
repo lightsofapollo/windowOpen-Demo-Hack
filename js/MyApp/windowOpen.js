c664a5d8a09c485a3957cb36cae65ba0cc64fd90
C Lanam <wht_rbt13@yahoo.com>
2011-10-03 16:17:32 -0700

    Another futile attempt to raze the tabs.

--- js/MyApp/windowOpen.js
+++ js/MyApp/windowOpen.js
@@ -1,45 +1,45 @@
 MyApp.windowOpen = (function(){
-	var Y = YAHOO;
-	var Event = Y.util.Event;
-	var Element = Y.util.Element;
-	var Dom = Y.util.Dom;
+    var Y = YAHOO;
+    var Event = Y.util.Event;
+    var Element = Y.util.Element;
+    var Dom = Y.util.Dom;
 
-	
-	return {		
-		init: function(windowClass){
-			if(undefined === windowClass){
-				windowClass = 'windowOpen';
-			}
-			
-			
-			var self = this;
-			Event.onDOMReady(function(){
-				self.initializeElements(windowClass);
-			});
-		},
-		
-		initializeElements: function(windowClass){
-			var elements = Dom.getElementsByClassName(windowClass), i = 0, length, element;
-			length = elements.length;
-						
-			for(var i = 0; i < length; i++){
-				element = elements[i];
-				this.bindElement(element);
-			}
-		},
-		
-		bindElement: function(element){
-			Event.addListener(element, 'click', function(e, obj){
-				Event.preventDefault(e);				
-				var href = this.href, 
-						newWin,
-						title = this.title, 
-						classes = this.className;
-										
-				newWin = window.open(href, title, 'height=200,width=550');
-				
-			}, element);
-		}
-		
-	}
+    
+    return {        
+        init: function(windowClass){
+            if(undefined === windowClass){
+                windowClass = 'windowOpen';
+            }
+            
+            
+            var self = this;
+            Event.onDOMReady(function(){
+                self.initializeElements(windowClass);
+            });
+        },
+        
+        initializeElements: function(windowClass){
+            var elements = Dom.getElementsByClassName(windowClass), i = 0, length, element;
+            length = elements.length;
+                        
+            for(var i = 0; i < length; i++){
+                element = elements[i];
+                this.bindElement(element);
+            }
+        },
+        
+        bindElement: function(element){
+            Event.addListener(element, 'click', function(e, obj){
+                Event.preventDefault(e);                
+                var href = this.href, 
+                        newWin,
+                        title = this.title, 
+                        classes = this.className;
+                                        
+                newWin = window.open(href, title, 'height=200,width=550');
+                
+            }, element);
+        }
+        
+    }
 }());
